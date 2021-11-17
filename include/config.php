3f9757e3c8ed7c4b24b1b45ff793a8491c20bfab
zehiyazerin <akhnirafkaa@gmail.com>
2021-11-17 13:29:34 +0000

    MangoSpot Fix

---
+++ include/config.php
@@ -0,0 +1,14 @@
+<?php
+define("TYPE", "mysql"); // pgsql or mysql
+define("DB_HOST", "localhost"); // localhost or ip
+define("DB_USER", "radius"); // username database
+define("DB_PASSWORD", "radius1234"); // password database
+define("DB_DATABASE", "radius"); // table database
+define("SSH_HOST", $_SERVER['SERVER_NAME']); // SSH Host
+define("SSH_PORT", 22); // Port SSH
+define("SSH_USERNAME", "akhnirafkaa"); // Username SSH
+define("SSH_PASSWORD", "Akhmad29"); // Password SSH
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'connect.php');
+?>
