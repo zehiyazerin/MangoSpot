2023812404a1682d7462c2422ac39834b069d89f
zehiyazerin <94539218+zehiyazerin@users.noreply.github.com>
2021-11-17 20:43:02 +0700

    Update config.php

--- include/config.php
+++ include/config.php
@@ -6,8 +6,8 @@ define("DB_PASSWORD", "radius1234"); // password database
 define("DB_DATABASE", "radius"); // table database
 define("SSH_HOST", $_SERVER['SERVER_NAME']); // SSH Host
 define("SSH_PORT", 22); // Port SSH
-define("SSH_USERNAME", "akhnirafkaa"); // Username SSH
-define("SSH_PASSWORD", "Akhmad29"); // Password SSH
+define("SSH_USERNAME", "username"); // Username SSH
+define("SSH_PASSWORD", "password"); // Password SSH
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'connect.php');
